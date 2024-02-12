7173fa3a06113e756b8e1d52bbe890ac3251f3c5
yzrh42 <aobwest@gmail.com>
2024-02-12 23:53:24 +0900

    LINEログイン設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_27_071939) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_12_140144) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +19,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_27_071939) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
