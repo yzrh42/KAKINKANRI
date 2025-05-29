92d501622b27fa55789edf561c4078833e5d1ad4
yzrh42 <aobwest@gmail.com>
2025-05-29 19:18:09 +0900

    新規登録のviewを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
     add_flash_types :success, :info, :warning, :danger
     before_action :authenticate_user!
+    protect_from_forgery with: :null_session
 end
